b07a948b4f5c91964b64761f59bc9dc11af827ab
hhurz <hhurz@online.de>
2016-06-15 16:50:24 +0200

    Added some accessibility attributes

--- js/jquery.colorPicker.js
+++ js/jquery.colorPicker.js
@@ -1,28 +1,10 @@
 /**
- * Really Simple Color Picker in jQuery
  *
  * Licensed under the MIT (MIT-LICENSE.txt) licenses.
  *
  * Copyright (c) 2008-2012
  * Lakshan Perera (www.laktek.com) & Daniel Lacy (daniellacy.com)
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
  */
 
 (function ($) {
@@ -31,9 +13,7 @@
     **/
     var selectorOwner,
         activePalette,
-        cItterate       = 0,
         templates       = {
-            control : $('<div class="colorPicker-picker">&nbsp;</div>'),
             palette : $('<div id="colorPicker_palette" class="colorPicker-palette" />'),
             swatch  : $('<div class="colorPicker-swatch">&nbsp;</div>'),
             hexLabel: $('<label for="colorPicker_hex">Hex</label>'),
@@ -154,8 +134,6 @@
             } else {
                 element.hide();
             }
-
-            cItterate++;
         });
     };
 
